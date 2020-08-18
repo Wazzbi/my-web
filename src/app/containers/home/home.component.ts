df9886202146ea44c5699d390cf1a6857bd61690
David Novotny <davidnovotny47@gmail.com>
2020-08-18 18:55:50 +0200

    uprava home page

--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -8,15 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   // https://openbase.io/js/ng-simple-slideshow
   imageUrlArray = [
-    '../../../assets/images/1.jpg',
-    '../../../assets/images/2.jpg',
-    '../../../assets/images/3.jpg',
-    '../../../assets/images/4.jpg',
-    '../../../assets/images/5.jpg',
-    '../../../assets/images/6.jpg',
-    '../../../assets/images/7.jpg',
-    '../../../assets/images/8.jpg',
-    '../../../assets/images/9.jpg'
+    '../../../assets/images/angular.png',
+    '../../../assets/images/vs-code.png',
+    '../../../assets/images/javascript.png',
+    '../../../assets/images/html5.png',
+    '../../../assets/images/css3.png',
+    '../../../assets/images/bootstrap.png',
+    '../../../assets/images/github.png',
+    '../../../assets/images/gitlab.png',
+    '../../../assets/images/ngrx.png',
+    '../../../assets/images/rxjs.png',
+    '../../../assets/images/sass.png'
+
   ];
 
   constructor() { }
